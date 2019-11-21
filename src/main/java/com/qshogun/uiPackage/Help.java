a13ede645c873488e0c28b53deda941013d3e273
qshogun <55805051+qshogun@users.noreply.github.com>
2019-11-21 16:49:12 +0100

    refactored xpaths

--- src/main/java/com/qshogun/uiPackage/Help.java
+++ src/main/java/com/qshogun/uiPackage/Help.java
@@ -2,22 +2,22 @@ package com.qshogun.uiPackage;
 
 public class Help {
     //FlightFinderPage web elements xpaths
-    public static final String roundTripXpath = "/html[1]/body[1]/div[1]/table[1]/tbody[1]/tr[1]/td[2]/table[1]/tbody[1]/tr[4]/td[1]/table[1]/tbody[1]/tr[1]/td[2]/table[1]/tbody[1]/tr[5]/td[1]/form[1]/table[1]/tbody[1]/tr[2]/td[2]/b[1]/font[1]/input[1]";
-    public static final String onewayTripXpath = "/html[1]/body[1]/div[1]/table[1]/tbody[1]/tr[1]/td[2]/table[1]/tbody[1]/tr[4]/td[1]/table[1]/tbody[1]/tr[1]/td[2]/table[1]/tbody[1]/tr[5]/td[1]/form[1]/table[1]/tbody[1]/tr[2]/td[2]/b[1]/font[1]/input[2]";
+    public static final String roundTripXpath = "//input[@name='tripType' and @value='roundtrip']";
+    public static final String onewayTripXpath = "//input[@name='tripType' and @value='oneway']";
     public static final String serviceClassXpath = ".//*[@value='Business']";
 
 
     //airlines xpaths
-    public static final String blueSkiesAirlines360Xpath = "/html[1]/body[1]/div[1]/table[1]/tbody[1]/tr[1]/td[2]/table[1]/tbody[1]/tr[4]/td[1]/table[1]/tbody[1]/tr[1]/td[2]/table[1]/tbody[1]/tr[5]/td[1]/form[1]/table[1]/tbody[1]/tr[3]/td[1]/input[1]";
-    public static final String blueSkiesAirlines361Xpath = "/html[1]/body[1]/div[1]/table[1]/tbody[1]/tr[1]/td[2]/table[1]/tbody[1]/tr[4]/td[1]/table[1]/tbody[1]/tr[1]/td[2]/table[1]/tbody[1]/tr[5]/td[1]/form[1]/table[1]/tbody[1]/tr[5]/td[1]/input[1]";
-    public static final String blueSkiesAirlines630Xpath = "/html[1]/body[1]/div[1]/table[1]/tbody[1]/tr[1]/td[2]/table[1]/tbody[1]/tr[4]/td[1]/table[1]/tbody[1]/tr[1]/td[2]/table[1]/tbody[1]/tr[5]/td[1]/form[1]/table[2]/tbody[1]/tr[3]/td[1]/input[1]";
-    public static final String blueSkiesAirlines631Xpath = "/html[1]/body[1]/div[1]/table[1]/tbody[1]/tr[1]/td[2]/table[1]/tbody[1]/tr[4]/td[1]/table[1]/tbody[1]/tr[1]/td[2]/table[1]/tbody[1]/tr[5]/td[1]/form[1]/table[2]/tbody[1]/tr[5]/td[1]/input[1]";
-    public static final String pangaeaAirlines362Xpath = "/html[1]/body[1]/div[1]/table[1]/tbody[1]/tr[1]/td[2]/table[1]/tbody[1]/tr[4]/td[1]/table[1]/tbody[1]/tr[1]/td[2]/table[1]/tbody[1]/tr[5]/td[1]/form[1]/table[1]/tbody[1]/tr[7]/td[1]/input[1]";
-    public static final String pangaeaAirlines632Xpath = "/html[1]/body[1]/div[1]/table[1]/tbody[1]/tr[1]/td[2]/table[1]/tbody[1]/tr[4]/td[1]/table[1]/tbody[1]/tr[1]/td[2]/table[1]/tbody[1]/tr[5]/td[1]/form[1]/table[2]/tbody[1]/tr[7]/td[1]/input[1]";
-    public static final String unifiedAirlines363Xpath = "/html[1]/body[1]/div[1]/table[1]/tbody[1]/tr[1]/td[2]/table[1]/tbody[1]/tr[4]/td[1]/table[1]/tbody[1]/tr[1]/td[2]/table[1]/tbody[1]/tr[5]/td[1]/form[1]/table[1]/tbody[1]/tr[9]/td[1]/input[1]";
-    public static final String unifiedAirlines633Xpath = "/html[1]/body[1]/div[1]/table[1]/tbody[1]/tr[1]/td[2]/table[1]/tbody[1]/tr[4]/td[1]/table[1]/tbody[1]/tr[1]/td[2]/table[1]/tbody[1]/tr[5]/td[1]/form[1]/table[2]/tbody[1]/tr[9]/td[1]/input[1]";
+    public static final String blueSkiesAirlines360Xpath = "//input[@type='radio' and contains(@value,'360')]";
+    public static final String blueSkiesAirlines361Xpath = "//input[@type='radio' and contains(@value,'361')]";
+    public static final String blueSkiesAirlines630Xpath = "//input[@type='radio' and contains(@value,'630')]";
+    public static final String blueSkiesAirlines631Xpath = "//input[@type='radio' and contains(@value,'631')]";
+    public static final String pangaeaAirlines362Xpath = "//input[@type='radio' and contains(@value,'362')]";
+    public static final String pangaeaAirlines632Xpath = "//input[@type='radio' and contains(@value,'632')]";
+    public static final String unifiedAirlines363Xpath = "//input[@type='radio' and contains(@value,'363')]";
+    public static final String unifiedAirlines633Xpath = "//input[@type='radio' and contains(@value,'633')]";
     public static final String continueButtonXpath = "//input[@name='reserveFlights']";
 
     //flight confirmation page
-    public static final String logoutButtonXpath = "/html[1]/body[1]/div[1]/table[1]/tbody[1]/tr[1]/td[2]/table[1]/tbody[1]/tr[4]/td[1]/table[1]/tbody[1]/tr[1]/td[2]/table[1]/tbody[1]/tr[7]/td[1]/table[1]/tbody[1]/tr[1]/td[3]/a[1]/img[1]";
+    public static final String logoutButtonXpath = "//img[contains[@src,'Logout'])";
 }
