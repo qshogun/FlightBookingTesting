9930179467bc87df144f776afee2b535e64f485c
qshogun <55805051+qshogun@users.noreply.github.com>
2019-11-21 17:16:43 +0100

    added tests: * checks if website opens * try to log in with correct username and incorrect password (1 lowercase letter)
    temporarly changed testng.Assert to junit.Assert
    
    added variables:
    * title of sign-on page
    * incorrect password: lowercase letter

--- src/main/java/com/qshogun/testPackage/DataClass.java
+++ src/main/java/com/qshogun/testPackage/DataClass.java
@@ -6,9 +6,13 @@ public class DataClass {
     public static final String websiteLink = "http://newtours.demoaut.com/";
     public static final String homepageTitle = "Welcome: Mercury Tours";
     public static final String afterLoginPageTitle = "Find a Flight: Mercury Tours:";
+    public static final String signonPageTitle = "Sign-on: Mercury Tours";
 
     //login credentials - correct
     public static final String usernameCorrect = "qshogun";
     public static final String passwordCorrect = "Asdf1234";
+
+    //login credentials - incorrect password #1
+    public static final String passwordIncorrectLowercase = "asdf1234";
 
 }
--- src/main/java/com/qshogun/testPackage/TestCaseClass.java
+++ src/main/java/com/qshogun/testPackage/TestCaseClass.java
@@ -1,10 +1,9 @@
 package com.qshogun.testPackage;
 
 import com.qshogun.uiPackage.LoginPage;
+import org.junit.Assert;
 import org.openqa.selenium.support.PageFactory;
-import org.testng.Assert;
 import org.testng.annotations.Test;
-//import org.junit.Test;
 
 import static com.qshogun.testPackage.DataClass.*;
 
@@ -15,11 +14,28 @@ public class TestCaseClass extends HelperClass {
     }
 
     @Test
+    public void checkIfWebsiteOpens() throws Exception {
+        System.out.println("in checkIfWebsiteOpens");
+        driver.get(websiteLink);
+        org.junit.Assert.assertEquals(homepageTitle, driver.getTitle());
+    }
+
+    @Test
     public void loginCredentialsCorrect() throws Exception {
         System.out.println("in loginCredentialsCorrect");
         driver.get(websiteLink);
         LoginPage loginPage = PageFactory.initElements(driver, LoginPage.class);
         loginPage.loginWordPress(usernameCorrect, passwordCorrect);
-        Assert.assertEquals(afterLoginPageTitle, driver.getTitle());
+        org.junit.Assert.assertEquals(afterLoginPageTitle, driver.getTitle());
     }
+
+    @Test
+    public void loginCredentialsIncorrect() throws Exception {
+        System.out.println("in loginCredentialsIncorrect");
+        driver.get(websiteLink);
+        LoginPage loginPage = PageFactory.initElements(driver, LoginPage.class);
+        loginPage.loginWordPress(usernameCorrect, passwordIncorrectLowercase);
+        org.junit.Assert.assertEquals(signonPageTitle, driver.getTitle());
+    }
+
 }
